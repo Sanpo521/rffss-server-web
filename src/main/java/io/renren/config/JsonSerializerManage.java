bfb54ef5c090403fd266374bb82e23e538240f93
zhuzhibo <12345678>
2023-08-29 10:45:31 +0800

    序列换成json时,将所有的long变成string

---
+++ src/main/java/io/renren/config/JsonSerializerManage.java
@@ -0,0 +1,35 @@
+package io.renren.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@JsonComponent
+public class JsonSerializerManage {
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        //忽略value为null 时 key的输出
+//        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        /**
+         * 序列换成json时,将所有的long变成string
+         * 因为js中得数字类型不能包含所有的java long值
+         */
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(module);
+        //所有字段返回字符串
+//        objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
+//            @Override
+//            public void serialize(Object o, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+//                jsonGenerator.writeString("");
+//            }
+//        });
+        return objectMapper;
+    }
+}
